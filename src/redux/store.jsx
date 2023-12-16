5ec39d52f21f7394038f4be11d906ff21f204023
dn280786lea <ekaterina.lubenets@gmail.com>
2023-12-16 19:30:41 +0100

    up
    up

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,24 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import { contactsReducer } from './contacts';
-import { filtersReducer } from './filtersSlice';
+import { reducer } from './reducer';
 
 export const store = configureStore({
-  reducer: { contacts: contactsReducer, filter: filtersReducer.reducer },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer,
 });
-
-export const persistor = persistStore(store);
